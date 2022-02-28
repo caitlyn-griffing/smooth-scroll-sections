9de7427a923a26e212be5ad496d1e26481a31a0f
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-02-28 13:31:15 -0600

    Navbar finished for now

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
-import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements'
 import { FaBars } from 'react-icons/fa'
+import { 
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks, 
+    NavBtn, 
+    NavBtnLink 
+} from './NavbarElements'
 
 const Navbar = () => {
   return (
